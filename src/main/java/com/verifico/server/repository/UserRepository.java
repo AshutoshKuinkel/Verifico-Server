32b5672c31920101a8d023cacd44b2b8a95eecf8
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-12-31 19:19:45 +1100

    feat: Create user/fetch user controllers setup

---
+++ src/main/java/com/verifico/server/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.verifico.server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.verifico.server.model.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+  
+}
