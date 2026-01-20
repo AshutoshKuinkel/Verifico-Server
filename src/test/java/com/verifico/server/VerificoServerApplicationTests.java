1a627e74407567fc853c880dd36ea6369c1a39b5
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-01-21 09:03:38 +1100

    TO DO: implement test containers

--- src/test/java/com/verifico/server/VerificoServerApplicationTests.java
+++ src/test/java/com/verifico/server/VerificoServerApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+// implement test containers for CI so we don't need to spin up db manually.
 class VerificoServerApplicationTests {
 
 	@Test
