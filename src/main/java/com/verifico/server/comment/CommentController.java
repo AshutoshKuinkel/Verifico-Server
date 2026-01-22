f64f9928253efa1f84e35cbc1842eb21cf06d2db
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-01-22 18:16:34 +1100

    to do: comment CRD operations

--- src/main/java/com/verifico/server/comment/CommentController.java
+++ src/main/java/com/verifico/server/comment/CommentController.java
@@ -1,19 +1,30 @@
 package com.verifico.server.comment;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.verifico.server.comment.dto.CommentRequest;
-import com.verifico.server.common.dto.APIResponse;
 
 @RestController
 @RequestMapping("/api/comments")
 public class CommentController {
 
   @PostMapping("/create/{id}")
-  public void createComment(CommentRequest request) {
+  public void createComment(@PathVariable("id") Long id, CommentRequest request) {
+
+  }
+
+  @GetMapping("/{id}")
+  public void fetchAllCommentsForPost(@PathVariable("id") Long id) {
+
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteComment(@PathVariable("id") Long id) {
 
   }
 }
--- src/main/java/com/verifico/server/comment/CommentService.java
+++ src/main/java/com/verifico/server/comment/CommentService.java
@@ -1,5 +1,33 @@
 package com.verifico.server.comment;
 
+import org.springframework.stereotype.Service;
+
+import com.verifico.server.comment.dto.CommentRequest;
+import com.verifico.server.post.PostRepository;
+
+// Create, read, delete operations only for now.
+// We won't feature edit/update comments initially.
+@Service
 public class CommentService {
-  
+
+  private CommentRepository commentRepository;
+
+  private PostRepository postRespository;
+
+  public CommentService(CommentRepository commentRepository) {
+    this.commentRepository = commentRepository;
+  }
+
+  public void postComment(CommentRequest request) {
+    
+  }
+
+  public void getAllCommentsForPost() {
+
+  }
+
+  public void deleteMyComment() {
+
+  }
+
 }
